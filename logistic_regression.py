6ca5d7d4eec533887b84b6f3c948307124471853
arnoldtan <tanatarnold@gmail.com>
2024-10-07 20:37:45 +0800

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,20 +1,21 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import roc_auc_score
 import pandas as pd
 
 train = pd.read_json('train_set_1.json', orient='records', lines=True)
 test = pd.read_json('test_set_1.json', orient='records', lines=True)
 
 trainY = train['label']
-trainX = train['-1_len':'+1_mean']
+trainX = train.loc[:, '-1_len':'+1_mean']
 testY = test['label']
-testX = test['-1_len':'+1_mean']
+testX = test.loc[:, '-1_len':'+1_mean']
 
 scalar = StandardScaler()
 trainXScaled = scalar.fit_transform(trainX)
+print(trainXScaled)
 
 model = LogisticRegression(random_state=1)
-model.fit(trainX, trainY)
+model.fit(trainXScaled, trainY)
 
 y_pred = model.predict(testX)
